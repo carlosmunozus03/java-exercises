986aa846e20806666bdb9c57ffea0924d559e270
Carlos Munoz <carlos.munoz.us03@gmail.com>
2021-09-28 16:04:11 -0500

    complete string lecture and exercises

---
+++ src/StringExercises.java
@@ -0,0 +1,24 @@
+public class StringExercises {
+    public static void main(String[] args) {
+        String education = "We don't need no education\n" +
+                "We don't need no thought control";
+        System.out.println(education);
+
+        System.out.println("-----------------------");
+
+        String check = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(check);
+
+        System.out.println("-----------------------");
+
+        String windows = "In windows, the main driver is usually C:\\";
+        System.out.println(windows);
+
+        System.out.println("-----------------------");
+
+        String slashes = "I can do backslashes \\, double backslashes \\\\, and the amazing triple backslash \\\\\\!";
+        System.out.println(slashes);
+
+        System.out.println("-----------------------");
+    }
+}

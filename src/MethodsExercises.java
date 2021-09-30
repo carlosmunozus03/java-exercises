9caab954fdb91da5f716108c4fa8ca905eed45f8
Carlos Munoz <carlos.munoz.us03@gmail.com>
2021-09-30 14:59:49 -0500

    Practice methods exercises

---
+++ src/MethodsExercises.java
@@ -0,0 +1,25 @@
+
+public class MethodsExercises {
+    public static void main(String[] args) {
+        add(2, 4);
+        subtract(4, 2);
+        multiply(3, 4);
+        divide(4, 2);
+    }
+
+    public static void add(long a, long b) {
+        System.out.println(a + b);
+    }
+
+    public static void subtract(long a, long b) {
+        System.out.println(a - b);
+    }
+
+    public static void multiply(long a, long b) {
+        System.out.println(a * b);
+    }
+
+    public static void divide(long a, long b) {
+        System.out.println(a / b);
+    }
+}

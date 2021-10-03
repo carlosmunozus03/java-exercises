4693176c73cc5e8ac9d7c52d2216e716a9ee5f9d
Carlos Munoz <carlos.munoz.us03@gmail.com>
2021-10-03 15:51:27 -0500

    Practice

--- src/Student.java
+++ src/Student.java
@@ -18,8 +18,28 @@ class Student {
 
     public static void main(String[] args) {
         Student s1 = new Student("Student A");
-        Student s2 = new Student("Student B", "Voyagers");
+        Student s2 = new Student("Student B", "Neptune");
         System.out.println(s1.getInfo()); // "name: Student A cohort: Unassigned"
-        System.out.println(s2.getInfo()); // "name: Student B cohort: Voyagers"
+        System.out.println(s2.getInfo()); // "name: Student B cohort: Neptune"
     }
 }
+
+//Using 'this'
+
+//class Student {
+//    public String name;
+//    public String cohort;
+//
+//    public Student(String name) {
+//        this(name, "Unassigned");
+//    }
+//
+//    public Student(String name, String cohort) {
+//        this.name = name;
+//        this.cohort = cohort;
+//    }
+//
+//    public String sayHello() {
+//        return "Hello from " + this.name + "!";
+//    }
+//}

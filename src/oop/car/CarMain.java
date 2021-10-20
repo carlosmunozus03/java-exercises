c5dd61ed48e43831a4b2870e577089098c403c32
Carlos Munoz <carlos.munoz.us03@gmail.com>
2021-10-20 17:46:36 -0500

    Continue practice with oop

--- src/oop/car/CarMain.java
+++ src/oop/car/CarMain.java
@@ -2,13 +2,23 @@ package oop.car;
 
 public class CarMain {
     public static void main(String[] args) {
-        Car ferrari = new Car();
 
-        ferrari.brand = "Ferrari";
-        ferrari.color = "Red";
-        ferrari.speed = 5;
+        Car ferrari = new Car("Red", "Ferrari", 300, 30000);
+        Car lamborghini = new Car("Yellow", "Lamborghini", 250, 20000);
 
-        ferrari.start("Ferrari", 0);
-        ferrari.stop();
+//        ferrari.brand = "Ferrari";
+//        ferrari.color = "Red";
+//        ferrari.speed = 5;
+
+        ferrari.start();
+        ferrari.off();
+
+//        System.out.println("Price of car is: USD$ " + ferrari.price);
+//        System.out.println("Max speed is: " + ferrari.speed + " km/hr");
+
+        //Using this in method
+        ferrari.move();
+        lamborghini.move();
+
     }
 }
